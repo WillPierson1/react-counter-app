82cdcd4b94000ed7ca900da2b2efd3efa600786e
Will <willmpierson@gmail.com>
2025-09-15 10:20:56 +0100

    HTML + Styling

--- src/components/CardItem.jsx
+++ src/components/CardItem.jsx
@@ -13,10 +13,28 @@ export default function CardItem(){
             {/* rate of click (+1 per click) */}
             <p>+1 per click</p>
             {/* Card Item Action Buttons */}
-            <button>-1</button>
-            <button>+1</button>
-            <button>Reset</button>
-            <button>Delete</button>
+            <div className='card-buttons'>
+                <button
+                    style={
+                        {backgroundColor: 'Orange'}
+                    }
+                >-1</button>
+                <button
+                    style={
+                        {backgroundColor: '#00e600'}
+                    }
+                >+1</button>
+                <button
+                    style={
+                        {backgroundColor: 'Grey'}
+                    }
+                >Reset</button>
+                <button
+                    style={
+                        {backgroundColor: 'Red'}
+                    }
+                >Delete</button>
+            </div>
         </div>
     )
 
